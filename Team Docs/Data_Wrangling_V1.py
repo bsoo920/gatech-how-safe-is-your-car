3582d0ecbc6d8993791770c05f5365bf897487e1
Captain Lenahan <kevinmcclenahan77@gmail.com>
2022-04-14 15:19:59 -0700

    Test

--- Team Docs/Data_Wrangling_V1.py
+++ Team Docs/Data_Wrangling_V1.py
@@ -3,9 +3,11 @@ import csv as csv
 import numpy as np
 import pandas as pd
 from IPython.display import display
-#todo: age of vehicle column
-#todo:addstate,state#,yearofaccidenttoeachcsvNHTSA,case#
 
+start_yr = 2016
+end_yr = 2020
+
+#TODO: BRIGHT: get 2005-2020 fars data
 #easily creates a name list for pulling in csv filenames
 def file_namer(startYR, endYR, name):
     #number range in list format
@@ -25,30 +27,32 @@ def get_setlist(filenames, col):
 
         df_final = pd.concat([df_final, df], ignore_index=True)
     return set(df_final[col])
-makemod_filenames = file_namer(2016, 2020, '')
+makemod_filenames = file_namer(start_yr, end_yr, '')
 make_set = get_setlist(makemod_filenames, col='make')
 print(make_set)
 mod_set = get_setlist(makemod_filenames, col='model')
 print(mod_set)
 
-def Load_NHTSA_Data(filenames):
-
+def Load_NHTSA_Data(filenames, start_yr):
+    # todo: addstate,state#,yearofaccidenttoeachcsvNHTSA,case#
+    # todo: age of vehicle column
     old_cols = ['MOD_YEAR', 'MAKENAME', 'MAK_MODNAME', 'BODY_TYP', 'DEATHS']
-    df_final = pd.DataFrame(columns=['Year', 'Make', 'Model', 'BODY_TYP'])
+    df_final = pd.DataFrame(columns=['mod_year', 'Make', 'Model', 'BODY_TYP'])
     # read through each filename in filenames
     for file in filenames:
         df = pd.read_csv(file, encoding='latin1', header=0, usecols=old_cols).astype(str)
         # print(df.isna().sum())
-        df.rename(columns={"MOD_YEAR": "Year",
+        df.rename(columns={"MOD_YEAR": "mod_year",
                            "MAKENAME": "Make",
                            "MAK_MODNAME": "Model",
+                           "DEATHS": "Deaths"
                            }, inplace=True)
-
-        df['DEATHS'] = df["DEATHS"].astype('int32') #Convert deaths col to int
+        # TODO: melirose: get sales data, and integer column for deaths
+        df['Deaths'] = df["Deaths"].astype('int32') #Convert deaths col to int
         df = df[~df['Make'].str.contains("Unknown")] # filter out Unknown rows
-        df = df[df['DEATHS'] > 0] #Filter for fatalities only
-        df.drop('DEATHS', axis=1, inplace=True) #drop death column
-
+        df = df[df['Deaths'] > 0] #Filter for fatalities only
+        df.drop('Deaths', axis=1, inplace=True) #drop death column
+        df["case_year"] = start_yr
         for i, v in enumerate(df["Model"]):
             print(i)
             print(v)
@@ -56,8 +60,6 @@ def Load_NHTSA_Data(filenames):
             #    df["Model"]
             break
 
-        # get model names separated into a new df
-        df["Model"] = df["Model"].str.replace(r'/', ' ')
 
         #df_makes = df["Model"].str.split(" ", n=2, expand=True)
         #print(df_makes.columns.tolist())
@@ -69,29 +71,31 @@ def Load_NHTSA_Data(filenames):
         #df['Model_1'] = df_makes[1]
 
         # kill white spaces across all cols
-        df = df.apply(lambda x: x.str.strip())
+        #df = df.apply(lambda x: x.str.strip())
 
         # deal with possible NAs
-        df['Year'].fillna('0', inplace=True)
+        df['mod_year'].fillna('0', inplace=True)
         df.fillna('unknown', inplace=True)
         #df['Model'].fillna('unknown', inplace=True)
         # create final df by concating the old with the new trimmed df
+        start_yr += 1
         df_final = pd.concat([df_final, df], ignore_index=True)
 
     #df_final.drop_duplicates(inplace=True, ignore_index=True)
     return df_final
 
+
 # list file names to concat into one df
 filenames = file_namer(2016, 2020, 'Vehicle')
-NHTSA_Data = Load_NHTSA_Data(filenames)
-#print(NHTSA_Data[0:10])
+NHTSA_Data = Load_NHTSA_Data(filenames, start_yr)
+print(NHTSA_Data[0:10])
 compression_opts = dict(method='zip', archive_name='NHTSA.csv')
 
-NHTSA_Data.to_csv('NHTSA.zip', index=False, compression=compression_opts)
+#NHTSA_Data.to_csv('NHTSA.zip', index=False, compression=compression_opts)
 
 def Load_Sales_Data(filename):
     keep_cols = ['Make', 'Model', '2016', '2017', '2018', '2019', '2020']
-    df = pd.read_csv('marklines2016-2021.csv', encoding='latin1', header=0, usecols=keep_cols).astype(str)
+    df = pd.read_csv('Car_sales_data.csv', encoding='latin1', header=0, usecols=keep_cols).astype(str)
     return df
 
 
