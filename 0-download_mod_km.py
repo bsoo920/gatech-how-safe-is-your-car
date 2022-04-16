2e2655023516d4c6c69d53a74ebe908ea7b204ab
Bright Soo <bsoo3@gatech.edu>
2022-04-15 18:16:20 -0700

    research work

--- 0-download_mod_km.py
+++ 0-download_mod_km.py
@@ -1,4 +1,6 @@
 import requests
+import csv
+from datetime import datetime
 import os
 import pandas as pd
 dir = 'NHTSA-FARS-download/'
@@ -9,26 +11,29 @@ model_list = []
 make_list = []
 year_list = []
 
-for year in range(2010, 2017):
+# Download from "make" API, which returns same data from 2010-2020 (except 2010 & 2011 had 2 fewer rows), and that pre-2010 gives errors.
+year=2020
+urlMakes = f"https://crashviewer.nhtsa.dot.gov/CrashAPI/definitions/GetVariableAttributes?variable=make&caseYear={year}&format=csv"
+r = requests.get(urlMakes, allow_redirects=True)
+open(dir+'make%d.csv' % (year), 'wb').write(r.content)      # downloading for reference only
+print(year, 'success', datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
 
-    for make in range(1, 100):
+makes = csv.reader( r.text.splitlines())
+next(makes)     #skip header row
+for [makeID, make, _, _] in makes:
+    # makes when iterated through:
+    # ['id', 'text', 'from_year', 'to_year']
+    # ['54', 'Acura', '1994', '']
+    # ['31', 'Alfa Romeo', '1994', '']
 
-        url = "https://crashviewer.nhtsa.dot.gov/CrashAPI/definitions/GetVariableAttributesForModel?variable=model&caseYear=%d&make=%d&format=json" % (year, make)
-        r = requests.get(url, allow_redirects=True)
-        data = r.json()
-        results = data['Results'][0]
+    # models API is also cumulative in nature, so just final year needed.
+    year=2020
 
-        for element in results:
-            id_list.append(element['ID'])
-        for element in results:
-            model_list.append(element['MODELNAME'])
-        for element in results:
-            make_list.append(make)
-        for element in results:
-            year_list.append(year)
+    url = f"https://crashviewer.nhtsa.dot.gov/CrashAPI/definitions/GetVariableAttributesForModel?variable=model&caseYear={year}&make={makeID}&format=csv"
+    rModels = requests.get(url, allow_redirects=True)
 
-df = pd.DataFrame(list(zip(id_list, make_list, model_list, year_list)), columns=['ID', 'Make_ID', 'Model', 'Case_Year'])
-df.to_csv('query_models.csv', index=False)
+    # os.makedirs(dir+make, exist_ok = True)
+    open( f"{dir}models/{make}_models{year}.csv", "wb" ).write(rModels.content)
+    print(year, makeID, make, 'success', datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
 
-
-
+    break
